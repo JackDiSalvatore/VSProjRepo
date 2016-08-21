80d5135ed5d10d12bc03e3d8db2204ce3a064dda
JackDiSalvatore <jdisalvatore7@gmail.com>
2016-08-21 18:58:08 -0400

    Multi parameter function template

--- CppTutorials/Function_Templates/Function_Templates/main.cpp
+++ CppTutorials/Function_Templates/Function_Templates/main.cpp
@@ -8,12 +8,24 @@ bucky addCrap(bucky a, bucky b)
 	return a + b;
 }
 
+
+template <class F, class S>
+F smaller(F a, S b)
+{
+	return (a < b ? a : b);
+}
+
+
 int main(int argc, char* argv[])
 {
 	int x = 7, y = 53, z;
+	int w = 150;
+	double q = 45.76;
 	
 	z = addCrap(x, y);
 	cout << z << endl;
+
+	cout << smaller(q, w) << endl;
 
 	return 0;
 }
