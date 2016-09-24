a1d38c7069ab07b72df59b1e3d1b367fbf4bbcc0
JackDiSalvatore <jdisalvatore7@gmail.com>
2016-09-24 17:34:30 -0400

    More on exceptions
    The "..." elipsices are used to catch any possible error thrown in the try block

--- CppTutorials/Execptions/Execptions/main.cpp
+++ CppTutorials/Execptions/Execptions/main.cpp
@@ -7,7 +7,8 @@ int main()
 	int motherAge = 34;
 	int sonAge = 30;
 
-	try {
+	try
+	{
 		if (sonAge > motherAge)
 		{
 			throw 1;
@@ -16,6 +17,33 @@ int main()
 	catch (int x)
 	{
 		cout << "ERROR: " << x << " Sons age can't be older then mothers" << endl;
+	}
+
+
+	try
+	{
+		int num1;
+		cout << "Enter first number: " << endl;
+		cin >> num1;
+
+		int num2;
+		cout << "Enter second number: " << endl;
+		cin >> num2;
+
+		if (0 == num2)
+		{
+			throw 0;
+		}
+		else
+		{
+			cout << num1 / num2 << endl;
+		}
+	}
+	// ... is used the catch any error message that could possibly be thrown above
+	// this is usefully if you have multiple throws that could be caught
+	catch (...)
+	{
+		cout << "You can't divide by zero!" << endl;
 	}
 
 	return 0;
